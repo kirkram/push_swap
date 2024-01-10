abe7b7e97faf911ebc1e3c231561c7121b5800aa
kirkram <94983351+kirkram@users.noreply.github.com>
2024-01-10 19:35:14 +0200

    it doesnt work at all, calling it a day for noww...

--- checker.c
+++ checker.c
@@ -6,17 +6,17 @@
 /*   By: klukiano <klukiano@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:08:11 by klukiano          #+#    #+#             */
-/*   Updated: 2024/01/10 16:50:09 by klukiano         ###   ########.fr       */
+/*   Updated: 2024/01/10 19:34:41 by klukiano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdlib.h>
 
-int		checker_p_swap(t_list **stack_a, t_list **stack_b, char **av);
-int		check_rot_op(t_list **stack_a, t_list **stack_b, char *buf);
-int		check_push_op(t_list **stack_a, t_list **stack_b, char *buf);
-int		check_swap_op(t_list **stack_a, t_list **stack_b, char *buf);
+int		checker_p_swap(t_list **stack_a, t_list **stack_b);
+int		check_rot_op(char *buf);
+int		check_push_op(char *buf);
+int		check_swap_op(char *buf);
 
 void	apply_sa(t_list **stack_a);
 void	apply_sb(t_list **stack_b);
@@ -30,13 +30,16 @@ void	apply_rra(t_list **stack_a);
 void	apply_rrb(t_list **stack_b);
 void	apply_rrr(t_list **stack_a, t_list **stack_b);
 
+void	apply_commands(t_list **stack_a, t_list **stack_astack_b, char *buf);
+
 void	print_current_stack(t_list *stack_a, t_list *stack_b);
 
 int main(int ac, char **av)
 {
-	t_list	*stack_a;
-	t_list	*stack_b;
-	int is_error;
+	t_list		*stack_a;
+	t_list		*stack_b;
+	int			is_error;
+	// static char	*buf = ft_calloc(600000, 1);
 
 	stack_b = NULL;
 	if (ac > 1 && check_input(ac, av))
@@ -46,13 +49,17 @@ int main(int ac, char **av)
 			ft_putstr_fd("Error\n", 2);
 		else
 		{
-			is_error = checker_p_swap(&stack_a, &stack_b, av);
+			is_error = checker_p_swap(&stack_a, &stack_b);
 			if (is_error)
 				ft_putstr_fd("Error\n", 2);
 			else if (is_sorted(stack_a) && !stack_b)
 				ft_printf("OK\n");
 			else if (!is_error)
+			{
+				//print_current_stack(stack_a, stack_b);
 				ft_printf("KO\n");
+			}
+
 		}
 	}
 	else if (ac == 1)
@@ -65,33 +72,49 @@ int main(int ac, char **av)
 
 //ccf checker_stack_operations.c checker_stack_operations_2.c helper_functions.c input_check.c checker.c libft.a create_stack.c -o checker
 
-int	checker_p_swap(t_list **stack_a, t_list **stack_b, char **av)
+int	checker_p_swap(t_list **stack_a, t_list **stack_b)
 {
-	char	*buf;
+	char	*buf = ft_calloc(600000, 1);
+	char	*ptr_buf;
 	int		is_error;
 
-	(void)av;
-	//print_current_stack(*stack_a, *stack_b);
+
 	is_error = 0;
-	buf = ft_calloc(42, 1);
+	//ft_strlcpy(buf, "pb\npb\npb\nrb\nrb\npb\npb\npb\nrb\npb\nrb\nrb\nrb\nrb\npb\nrb\nrr\npb\nra\nrrb\npb\nrb\nrrb\npb\nrrr\nrra\npb\nrra\npb\nrra\nrb\npb\nrrr\nrra\nrra\npb\nrrr\npb\nrrr\nrra\nrra\npb\nrb\nrb\nrb\npb\nrrr\nrrr\nrrr\npb\npb\nrrr\nrrr\nrrr\npb\nrrb\npb\nrb\npb\nrb\npb\nrb\nrb\nrb\npb\nrb\npb\nrb\nrb\npb\nrb\nrb\npb\nrrr\nrrr\nrrr\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\npb\nrb\nrr\nrr\nrb\npb\nrb\nrb\nrb\nrb\npb\npb\nrr\nrr\nra\nra\npb\nrr\nrb\nrb\npb\nrr\nrb\nrb\nrb\nrb\nrb\nrb\npb\nrrr\nrra\nrra\nrra\nrra\nrra\nrra\nrra\npb\nrrr\nrrr\nrrr\npb\nrrr\nrrr\nrrr\nrrr\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\npb\nrb\nrr\nrr\nrb\nrb\npb\nrb\nrb\nrb\npb\nrrr\nrrr\nrrr\nrra\nrra\nrra\npb\nrrr\nrra\nrra\npb\nrrr\nrrr\nrrr\nrrr\nrrr\nrra\npb\nrrr\nrrr\npb\nra\nra\nra\nra\nra\npb\nrrr\nrrr\npb\nrrb\npb\nrr\nrr\npb\nrrr\nrrb\nrrb\npb\nrrr\nrrb\npb\nrr\nra\nra\nra\npb\nrr\nrr\nrb\nrb\nrb\npb\nrr\nra\npb\nrr\nrr\nrb\nrb\npb\nrb\npb\nrra\nrra\nrra\nrra\nrra\npb\nrr\nrr\nrb\nrb\nrb\nrb\npb\nrra\nrra\nrra\nrb\npb\nrr\nrr\nrr\nrr\nrr\nrb\nrb\nrb\nrb\npb\nrr\nrr\nrr\nrb\npb\nrr\nrr\nrr\nrr\nrr\npb\nrr\nrr\nrr\nrr\npb\nrra\nrra\nrra\nrra\nrra\npb\nrra\nrb\nrb\nrb\nrb\npb\nrra\npb\nrra\nrra\nrb\npb\nrrr\nrrr\nrrr\nrrr\nrrr\npb\nra\npb\nrrr\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\npb\nrrr\npb\nrrr\nrrr\nrrb\nrrb\npb\nrr\nra\nra\nra\nra\nra\npb\nrr\nrr\nrr\nrr\npb\nrb\nrb\npb\nra\nra\nrrb\nrrb\nrrb\npb\nrrr\nrrr\nrrr\nrrr\nrrr\nrrb\nrrb\nrrb\npb\nrra\nrra\npb\nra\nra\nra\nrrb\nrrb\nrrb\nrrb\npb\nrrr\nrrr\nrrr\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\nrrb\npb\nra\npb\nrrr\nrrr\nrrr\nrrr\nrrr\nrrr\nrrb\nrrb\nrrb\nrrb\nrrb\npb\nrrr\nrrr\nrrr\nrrr\nrrr\nrrr\nrra\nrra\npb\nrrr\nrrb\npb\nrrr\nrrr\npb\nrr\npb\nra\npb\nra\nra\nra\nra\npb\nrrr\npb\nra\nra\npb\nra\nra\npb\nrb\nrb\nrb\nrb\nrb\nrb\npb\nrra\npb\nrra\npb\nrra\nrra\nrb\npb\nrb\nrb\nrb\nrb\npb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\npb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\npb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nrb\nsa\nrra\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\nrra\npa\npa\npa\npa\npa\npa\nrra\npa\npa\nrra\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa\npa", -1);
+	//ft_strlcpy(buf, "pb\npb\nsb\npa\npa", -1);
+	ptr_buf = buf;
 	while (42)
 	{
-		if (is_error || buf[0] == '\n')
+		read (0, ptr_buf, 4);
+		if (is_error || ptr_buf[0] == '\0')
 			break ;
-		read (0, buf, 4);
-		if ( buf[0] == '\0')
-			break ;
-		if (buf[2] != '\n' && buf[3] != '\n')
-			return (1);
-		else if (buf[0] == 'r')
-			is_error = check_rot_op(stack_a, stack_b, buf);
-		else if (buf[0] == 'p')
-			is_error = check_push_op(stack_a, stack_b, buf);
-		else if (buf[0] == 's')
-			is_error = check_swap_op(stack_a, stack_b, buf);
-		else if (buf[0] != '\n')
+		if (ptr_buf[2] != '\n' && ptr_buf[3] != '\n')
+		{
+			if (ptr_buf[2] != '\0' && ptr_buf[3] != '\0')
+				is_error = 1;
+		}
+		else if (ptr_buf[0] == 'r')
+			is_error = check_rot_op(ptr_buf);
+		else if (ptr_buf[0] == 'p')
+			is_error = check_push_op(ptr_buf);
+		else if (ptr_buf[0] == 's')
+			is_error = check_swap_op(ptr_buf);
+		else if (ptr_buf[0] != '\0')
 			is_error = 1;
+		if (ptr_buf[2] == '\n' || ptr_buf[2] == '\0')
+			ptr_buf = ptr_buf + 3;
+		else if (ptr_buf[3] == '\n' || ptr_buf[3] == '\0')
+			ptr_buf = ptr_buf + 4;
+		else
+			is_error = 1;
+		if (is_error)
+			break ;
 	}
+	ptr_buf[0] = '\0';
+
+
+	if (!is_error)
+		apply_commands(stack_a, stack_b, buf);
 	free (buf);
 	return (is_error);
 }
@@ -102,86 +125,99 @@ void	apply_ss(t_list **stack_a, t_list **stack_b)
 	apply_sb (stack_b);
 }
 
-int	check_rot_op(t_list **stack_a, t_list **stack_b, char *buf)
+int	check_rot_op(char *buf)
 {
-	if (buf[2] != '\n' && buf[3] != '\n')
-		return (1);
-	else if (!ft_strncmp(buf, "rrr", 3))
-	{
-		//ft_printf("PRINTING RRR\n");
-		apply_rrr(stack_a, stack_b);
-	}
+	if (!ft_strncmp(buf, "rrr", 3))
+		return (0);
 	else if (!ft_strncmp(buf, "rra", 3))
-	{
-		//ft_printf("PRINTING RRA\n");
-		apply_rra(stack_a);
-	}
+		return (0);
 	else if (!ft_strncmp(buf, "rrb", 3))
-	{
-		//ft_printf("PRINTING RRB\n");
-		apply_rrb(stack_b);
-	}
+		return (0);
 	else if (!ft_strncmp(buf, "rr", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING RR\n");
-		apply_rr(stack_a, stack_b);
-	}
+		return (0);
 	else if (!ft_strncmp(buf, "ra", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING RA\n");
-		apply_ra(stack_a);
-	}
+		return (0);
 	else if (!ft_strncmp(buf, "rb", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING RB\n");
-		apply_rb(stack_b);
-	}
+		return (0);
 	else
-	{
 		return (1);
-	}
-	return (0);
 }
 
-int	check_push_op(t_list **stack_a, t_list **stack_b, char *buf)
+int	check_push_op(char *buf)
 {
-	if (buf[2] != '\n' && buf[3] != '\n')
-		return (1);
-	else if (!ft_strncmp(buf, "pa", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING PA\n");
-		apply_pa(stack_a, stack_b);
-	}
+	if (!ft_strncmp(buf, "pa", 2) && !ft_isprint(buf[2]))
+		return (0);
 	else if (!ft_strncmp(buf, "pb", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING PB\n");
-		apply_pb(stack_a, stack_b);
-	}
+		return (0);
 	else
 		return (1);
-	return (0);
 }
 
-int	check_swap_op(t_list **stack_a, t_list **stack_b, char *buf)
+int	check_swap_op(char *buf)
 {
-	if (buf[2] != '\n' && buf[3] != '\n')
-		return (1);
-	else if (!ft_strncmp(buf, "sa", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING SA\n");
-		apply_sa(stack_a);
-	}
+	if (!ft_strncmp(buf, "sa", 2) && !ft_isprint(buf[2]))
+		return (0);
 	else if (!ft_strncmp(buf, "sb", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING SB\n");
-		apply_sb(stack_b);
-	}
+		return (0);
 	else if (!ft_strncmp(buf, "ss", 2) && !ft_isprint(buf[2]))
-	{
-		//ft_printf("PRINTING SS\n");
-		apply_ss(stack_a, stack_b);
-	}
+		return (0);
 	else
 		return (1);
-	return (0);
+}
+
+void	apply_commands(t_list **stack_a, t_list **stack_b, char *buf)
+{
+	int i;
+
+	//ft_printf("THE buf before is %s\n", buf);
+	//buf = "rra\npb\nsa\nrra\npa";
+	while (*buf)
+	{
+		i = 0;
+		while (buf[i] && buf[i] != '\n' )
+			i ++;
+		if (i == 3)
+		{
+			if (!ft_strncmp(buf, "rrr", 3))
+				apply_rrr(stack_a, stack_b);
+			else if (!ft_strncmp(buf, "rra", 3))
+				apply_rra(stack_a);
+			else if (!ft_strncmp(buf, "rrb", 3))
+				apply_rrb(stack_b);
+			buf += 4;
+		}
+		else if (i == 2)
+		{
+			if (!ft_strncmp(buf, "sa", 2))
+				apply_sa(stack_a);
+			else if (!ft_strncmp(buf, "sb", 2))
+				apply_sb(stack_a);
+			else if (!ft_strncmp(buf, "ss", 2))
+				apply_ss(stack_a, stack_b);
+			else if (!ft_strncmp(buf, "pa", 2))
+				apply_pa(stack_a, stack_b);
+			else if (!ft_strncmp(buf, "pb", 2))
+				apply_pb(stack_a, stack_b);
+			else if (!ft_strncmp(buf, "rr", 2))
+				apply_rr(stack_a, stack_b);
+			else if (!ft_strncmp(buf, "ra", 2))
+				apply_ra(stack_a);
+			else if (!ft_strncmp(buf, "rb", 2))
+				apply_rb(stack_b);
+			buf += 3;
+		}
+		else if (!buf[i])
+		{
+			//ft_printf("End of file\n");
+			break;
+		}
+		// else
+		// {
+		// 	// ft_printf("The I is now %d", i);
+		// 	// ft_printf("ERROR IN APPLY\n");
+		// 	break;
+		// }
+
+	}
+	return ;
 }
